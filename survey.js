cb0351643a7081b72cf42901b7af27d6d122a868
ABHINAV KUMAR PATHAK <abhiraj132560@gmail.com>
2023-12-01 19:40:36 +0530

    Add files via upload

---
+++ survey.js
@@ -0,0 +1,9 @@
+// Define a function to toggle the navigation menu
+function toggleNav() {
+    var nav = document.querySelector('.nav');
+    nav.classList.toggle('active');
+}
+
+function asa() {
+    alert("You are leaving this page");
+}
